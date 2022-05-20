c78c58bc15464b52d1c9bb2138bcd270df27304a
“4testaccount” <yang.mingke@foxmail.com>
2022-05-20 22:26:17 +0800

    add jupyter monitor

--- util/monitor.py
+++ util/monitor.py
@@ -14,8 +14,16 @@ class DataBase(object):
             time.sleep(2)
             self.__init__()
 
-    def query_question_count(self):
+    def query_so_count(self):
         query_sql = "select count(*) from so_code_snippet"
+        cursor = self.mysql.cursor()
+        cursor.execute(query_sql)
+        count = cursor.fetchone()[0]
+        cursor.close()
+        return count
+
+    def query_jupyter_count(self):
+        query_sql = "select count(*) from jupyter_code_snippet"
         cursor = self.mysql.cursor()
         cursor.execute(query_sql)
         count = cursor.fetchone()[0]
@@ -24,11 +32,18 @@ class DataBase(object):
 
 
 if __name__ == "__main__":
-    prv_count = 0
-    while (True):
+    # so_prv_count = 0
+    jupyter_count = 0
+    while True:
         db = DataBase()
-        cur_count = db.query_question_count()
-        print("diff size: " + str(cur_count - prv_count))
-        print("total size: " + str(cur_count))
-        prv_count = cur_count
+        # so_cur_count = db.query_so_count()
+        jupyter_cur_count = db.query_jupyter_count()
+        # print("so diff size: " + str(so_cur_count - so_prv_count))
+        # print("so total size: " + str(so_cur_count))
+
+        print("jupyter diff size: " + str(jupyter_cur_count - jupyter_count))
+        print("jupyter total size: " + str(jupyter_cur_count))
+        # so_prv_count = so_cur_count
+        jupyter_count = jupyter_cur_count
+        print("----------")
         time.sleep(20)
