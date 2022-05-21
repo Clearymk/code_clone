82651efefdc2f4962d53f65a866d4d65634cd54d
“4testaccount” <yang.mingke@foxmail.com>
2022-05-21 09:51:09 +0800

    add monitor clone pair feature

--- util/monitor.py
+++ util/monitor.py
@@ -30,20 +30,37 @@ class DataBase(object):
         cursor.close()
         return count
 
+    def query_clone_pair_count(self):
+        query_sql = "select count(*) from clone_pair"
+        cursor = self.mysql.cursor()
+        cursor.execute(query_sql)
+        count = cursor.fetchone()[0]
+        cursor.close()
+        return count
+
 
 if __name__ == "__main__":
-    # so_prv_count = 0
+    so_prv_count = 0
     jupyter_count = 0
+    clone_pair_count = 0
+
     while True:
         db = DataBase()
+
         # so_cur_count = db.query_so_count()
-        jupyter_cur_count = db.query_jupyter_count()
         # print("so diff size: " + str(so_cur_count - so_prv_count))
         # print("so total size: " + str(so_cur_count))
-
-        print("jupyter diff size: " + str(jupyter_cur_count - jupyter_count))
-        print("jupyter total size: " + str(jupyter_cur_count))
         # so_prv_count = so_cur_count
-        jupyter_count = jupyter_cur_count
+        #
+        # jupyter_cur_count = db.query_jupyter_count()
+        # print("jupyter diff size: " + str(jupyter_cur_count - jupyter_count))
+        # print("jupyter total size: " + str(jupyter_cur_count))
+        # jupyter_count = jupyter_cur_count
+
+        clone_pair_cur_count = db.query_jupyter_count()
+        print("clone pair diff size: " + str(clone_pair_cur_count - clone_pair_count))
+        print("clone pair total size: " + str(clone_pair_cur_count))
+        clone_pair_count = clone_pair_cur_count
+
         print("----------")
         time.sleep(20)
