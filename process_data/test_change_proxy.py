3b6febda2cf11f02d03ac4ddbea4f748cbc9a7fb
“4testaccount” <yang.mingke@foxmail.com>
2022-07-07 14:30:50 +0800

    change profile to naiko

--- process_data/test_change_proxy.py
+++ process_data/test_change_proxy.py
@@ -1,10 +1,14 @@
 import requests
 
-controller = "http://127.0.0.1:9090/proxies/"
-proxy_url = controller + "AmyTelecom"
+controller = "http://127.0.0.1:2390/proxies/"
+proxy_url = controller + "%F0%9F%9A%80%20%E8%8A%82%E7%82%B9%E9%80%89%E6%8B%A9"
 
 proxy_group = [
-    '香港 01', '香港 HK 02', '香港 HK 03', '香港 HK 04', '香港 HK 05', '香港 HK 06', '香港 HK 07', '香港 HK 08', '香港 HK 09',
+    '台湾 TW 01', '台湾 TW 02', '台湾 TW 03', '台湾 TW 04', '台湾 TW 05', '台湾 TW 06', '台湾 TW 07', '台湾 TW 08',
+    '新加坡 SG 01', '新加坡 SG 02', '新加坡 SG 03', '新加坡 SG 04', '新加坡 SG 05', '新加坡 SG 06', '新加坡 SG 07', '新加坡 SG 08',
+    '日本 JP 01', '日本 JP 02', '日本 JP 03', '日本 JP 04', '日本 JP 05', '日本 JP 06', '日本 JP 07', '日本 JP 08',
+    '韩国 KR 01', '韩国 KR 02', '韩国 KR 03', '韩国 KR 04', '韩国 KR 05', '韩国 KR 06', '韩国 KR 07', '韩国 KR 08',
+    '香港 HK 01', '香港 HK 02', '香港 HK 03', '香港 HK 04', '香港 HK 05', '香港 HK 06', '香港 HK 07', '香港 HK 08', '香港 HK 09',
     '香港 HK 10'
 ]
 
